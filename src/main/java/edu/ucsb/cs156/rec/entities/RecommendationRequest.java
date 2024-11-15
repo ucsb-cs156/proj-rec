7936a836644d9e157d56f3d63892daa6edf34371
Mike Petrus <Petrus@ucsb.edu>
2024-11-15 13:24:14 -0800

    E11-B1: Create RecReq Table

---
+++ src/main/java/edu/ucsb/cs156/rec/entities/RecommendationRequest.java
@@ -0,0 +1,35 @@
+package edu.ucsb.cs156.rec.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * This is a JPA entity that represents a Recommendation Request
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "recommendationrequest")
+public class RecommendationRequest {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  private String professorName;
+  private String professorEmail;
+  private String requesterName;
+  private String recommendationType;
+  private String details;
+
+  private LocalDateTime completionDate;
+}
