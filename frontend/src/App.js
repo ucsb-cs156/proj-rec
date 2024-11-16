cd59e3160fd5128bb23be9fbc8dc402ca062f303
Amber Wang <ywang116@ucsb.edu>
2024-11-16 02:06:30 -0600

    Added placeholders for Professor Pending/CompletedRequests/Statistics pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,10 @@ import PlaceholderIndexPage from "main/pages/Placeholder/PlaceholderIndexPage";
 import PlaceholderCreatePage from "main/pages/Placeholder/PlaceholderCreatePage";
 import PlaceholderEditPage from "main/pages/Placeholder/PlaceholderEditPage";
 
+import PendingRequestsPage from "main/pages/Requests/PendingRequestsPage";
+import CompletedRequestsPage from "main/pages/Requests/CompletedRequestsPage";
+import StatisticsPage from "main/pages/Requests/StatisticsPage";
+
 import { hasRole, useCurrentUser } from "main/utils/currentUser";
 
 import "bootstrap/dist/css/bootstrap.css";
@@ -93,6 +97,25 @@ function App() {
               exact
               path="/placeholder/create"
               element={<PlaceholderCreatePage />}
+            />
+          </>
+        )}
+        {hasRole(currentUser, "ROLE_PROFESSOR") && (
+          <>
+            <Route
+              exact
+              path="/requests/pending"
+              element={<PendingRequestsPage />}
+            />
+            <Route
+              exact
+              path="/requests/completed"
+              element={<CompletedRequestsPage />}
+            />
+            <Route
+              exact
+              path="/requests/statistics"
+              element={<StatisticsPage />}
             />
           </>
         )}
