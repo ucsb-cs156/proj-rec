0b61e38ecf62473b6d3c44a2e4dd3cc0989eae19
Logan Detrick <logandetrick@ucsb.edu>
2024-11-18 15:28:04 -0800

    ld - fix prettier

--- frontend/src/fixtures/requestTypeFixtures.js
+++ frontend/src/fixtures/requestTypeFixtures.js
@@ -1,28 +1,27 @@
 const requestTypeFixtures = {
-	oneRequestType: [
-	  {
-		id: 1,
-		requestType: "CS Department BS/MS program",
-	  },
-	],
-  
-	threeRequestType: [
-	  {
-		id: 2,
-		requestType: "Scholarship or Fellowship",
-	  },
-  
-	  {
-		id: 3,
-		requestType: "PhD program",
-	  },
-  
-	  {
-		id: 4,
-		requestType: "Other",
-	  },
-	],
-  };
-  
-  export { requestTypeFixtures };
-  
+  oneRequestType: [
+    {
+      id: 1,
+      requestType: "CS Department BS/MS program",
+    },
+  ],
+
+  threeRequestType: [
+    {
+      id: 2,
+      requestType: "Scholarship or Fellowship",
+    },
+
+    {
+      id: 3,
+      requestType: "PhD program",
+    },
+
+    {
+      id: 4,
+      requestType: "Other",
+    },
+  ],
+};
+
+export { requestTypeFixtures };
