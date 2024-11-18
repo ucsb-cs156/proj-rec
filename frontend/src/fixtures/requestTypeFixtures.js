d02e5a88871701ca387cd0c2ecf38e62d9287c99
Logan Detrick <logandetrick@ucsb.edu>
2024-11-18 15:23:43 -0800

    ld - create fixtures for RequestType

---
+++ frontend/src/fixtures/requestTypeFixtures.js
@@ -0,0 +1,28 @@
+const requestTypeFixtures = {
+	oneRequestType: [
+	  {
+		id: 1,
+		requestType: "CS Department BS/MS program",
+	  },
+	],
+  
+	threeRequestType: [
+	  {
+		id: 2,
+		requestType: "Scholarship or Fellowship",
+	  },
+  
+	  {
+		id: 3,
+		requestType: "PhD program",
+	  },
+  
+	  {
+		id: 4,
+		requestType: "Other",
+	  },
+	],
+  };
+  
+  export { requestTypeFixtures };
+  
