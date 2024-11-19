4f90d5f871b0cd17019a0f2cea3b01eb8aebc6b4
Ryan Zanone <ryanzanone@ucsb.edu>
2024-11-18 19:59:00 -0800

    Created entity file for RequestType db

---
+++ src/main/java/edu/ucsb/cs156/rec/entities/RequestType.java
@@ -0,0 +1,27 @@
+package edu.ucsb.cs156.rec.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * This is a JPA entity that represents a RequestType
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "requesttype")
+public class RequestType {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  private String requestType;
+}
