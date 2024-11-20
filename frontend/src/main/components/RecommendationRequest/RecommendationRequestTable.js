e04ba71a8a5001c901e2af5e1179da6b9747acdb
Divyani Punj <divyanipunj@ucsb.edu>
2024-11-19 22:57:46 -0800

    Added the table component and stories for RecommendationRequest.

---
+++ frontend/src/main/components/RecommendationRequest/RecommendationRequestTable.js
@@ -0,0 +1,83 @@
+import React from "react";
+import OurTable, { ButtonColumn } from "main/components/OurTable";
+
+import { useBackendMutation } from "main/utils/useBackend";
+import {
+  cellToAxiosParamsDelete,
+  onDeleteSuccess,
+} from "main/utils/UCSBDateUtils";
+import { useNavigate } from "react-router-dom";
+import { hasRole } from "main/utils/currentUser";
+
+export default function RecommendationRequestTable({ requests, currentUser }) {
+  const navigate = useNavigate();
+
+  const editCallback = (cell) => {
+    navigate(`/requests/edit/${cell.row.values.id}`);
+  };
+
+  // Stryker disable all : hard to test for query caching
+
+  const deleteMutation = useBackendMutation(
+    cellToAxiosParamsDelete,
+    { onSuccess: onDeleteSuccess },
+    ["/api/recommendationrequest/all"],
+  );
+  // Stryker restore all
+
+  // Stryker disable next-line all : TODO try to make a good test for this
+  const deleteCallback = async (cell) => {
+    deleteMutation.mutate(cell);
+  };
+
+  const columns = [
+    {
+      Header: "id",
+      accessor: "id", // accessor is the "key" in the data
+    },
+    {
+      Header: "Professor Name",
+      accessor: "professorName",
+    },
+    {
+      Header: "Professor Email",
+      accessor: "professorEmail",
+    },
+    {
+      Header: "Requester Name",
+      accessor: "requesterName",
+    },
+    {
+      Header: "Recommendation Types",
+      accessor: "recommendationTypes",
+    },
+    {
+      Header: "Details",
+      accessor: "details",
+    },
+    {
+      Header: "Status",
+      accessor: "status",
+    },
+    {
+      Header: "Submission Date",
+      accessor: "submissionDate",
+    },
+    {
+      Header: "Completion Date",
+      accessor: "completionDate",
+    },
+
+  ];
+
+  if (hasRole(currentUser, "ROLE_ADMIN")) {
+    columns.push(
+      ButtonColumn("Edit", "primary", editCallback, "RecommendationRequestTable"),
+    );
+    columns.push(
+      ButtonColumn("Delete", "danger", deleteCallback, "RecommendationRequestTable"),
+    );
+  }
+
+  return <OurTable data={requests} columns={columns} testid={"RecommendationRequestTable"} />;
+}
