b86f2d4fbfd892ae68fc400ccdc37ad2087c01b7
obenedek20 <obenedek20@users.noreply.github.com>
2024-11-19 20:45:45 -0800

    prettier

--- frontend/src/fixtures/recommendationRequestFixtures.js
+++ frontend/src/fixtures/recommendationRequestFixtures.js
@@ -1,56 +1,55 @@
 const recommendationRequestFixtures = {
-    oneRecommendation: [
-      {
-        id: 1,
-        professorName: "testname",
-        professorEmail: "testemail@ucsb.edu",
-        requesterName: "recname",
-        submissionDate: "2022-02-02T12:00",
-        completionDate: "2022-02-02T12:00",
-        status: "PENDING",
-        details: "test details",
-        recommendationTypes: "this type",
-      },
-    ],
-  
-    threeRecommendations: [
-      {
-        id: 2,
-        professorName: "testname1",
-        professorEmail: "testemail1@ucsb.edu",
-        requesterName: "recname1",
-        submissionDate: "2022-02-02T12:00",
-        completionDate: "2022-02-02T12:00",
-        status: "PENDING",
-        details: "test details",
-        recommendationTypes: "this type 1",
-      },
-  
-      {
-        id: 3,
-        professorName: "testname2",
-        professorEmail: "testemail2@ucsb.edu",
-        requesterName: "recname2",
-        submissionDate: "2022-02-02T12:00",
-        completionDate: "2022-02-02T12:00",
-        status: "COMPLETED",
-        details: "test details",
-        recommendationTypes: "this type 2",
-      },
-  
-      {
-        id: 4,
-        professorName: "testname3",
-        professorEmail: "testemail3@ucsb.edu",
-        requesterName: "recname3",
-        submissionDate: "2022-02-02T12:00",
-        completionDate: "2022-02-02T12:00",
-        status: "DENIED",
-        details: "test details",
-        recommendationTypes: "this type 3",
-      },
-    ],
-  };
-  
-  export { recommendationRequestsFixtures };
-  
+  oneRecommendation: [
+    {
+      id: 1,
+      professorName: "testname",
+      professorEmail: "testemail@ucsb.edu",
+      requesterName: "recname",
+      submissionDate: "2022-02-02T12:00",
+      completionDate: "2022-02-02T12:00",
+      status: "PENDING",
+      details: "test details",
+      recommendationTypes: "this type",
+    },
+  ],
+
+  threeRecommendations: [
+    {
+      id: 2,
+      professorName: "testname1",
+      professorEmail: "testemail1@ucsb.edu",
+      requesterName: "recname1",
+      submissionDate: "2022-02-02T12:00",
+      completionDate: "2022-02-02T12:00",
+      status: "PENDING",
+      details: "test details",
+      recommendationTypes: "this type 1",
+    },
+
+    {
+      id: 3,
+      professorName: "testname2",
+      professorEmail: "testemail2@ucsb.edu",
+      requesterName: "recname2",
+      submissionDate: "2022-02-02T12:00",
+      completionDate: "2022-02-02T12:00",
+      status: "COMPLETED",
+      details: "test details",
+      recommendationTypes: "this type 2",
+    },
+
+    {
+      id: 4,
+      professorName: "testname3",
+      professorEmail: "testemail3@ucsb.edu",
+      requesterName: "recname3",
+      submissionDate: "2022-02-02T12:00",
+      completionDate: "2022-02-02T12:00",
+      status: "DENIED",
+      details: "test details",
+      recommendationTypes: "this type 3",
+    },
+  ],
+};
+
+export { recommendationRequestFixtures };
