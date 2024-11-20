a48dff5bcfc0d38fdb41dc546487261ea2b49a1c
Edward Garcia <edwardgarcia@ucsb.edu>
2024-11-19 17:15:43 -0800

    WIP - UsersController.java

--- src/main/java/edu/ucsb/cs156/rec/controllers/UsersController.java
+++ src/main/java/edu/ucsb/cs156/rec/controllers/UsersController.java
@@ -6,9 +6,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import edu.ucsb.cs156.rec.errors.EntityNotFoundException;
 
 import edu.ucsb.cs156.rec.entities.User;
 import edu.ucsb.cs156.rec.repositories.UserRepository;
@@ -44,5 +49,68 @@ public class UsersController extends ApiController {
         Iterable<User> users = userRepository.findAll();
         String body = mapper.writeValueAsString(users);
         return ResponseEntity.ok().body(body);
+    }
+
+    @Operation(summary = "Get user by id")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/get")
+    public User users(
+            @Parameter(name = "id", description = "Long, id number of user to get", example = "1", required = true) @RequestParam Long id)
+            throws JsonProcessingException {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(User.class, id));
+        return user;
+    }
+
+    @Operation(summary = "Delete a user (admin)")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("/delete")
+    public Object deleteUser_Admin(
+            @Parameter(name = "id", description = "Long, id number of user to delete", example = "1", required = true) @RequestParam Long id) {
+              User user = userRepository.findById(id)
+          .orElseThrow(() -> new EntityNotFoundException(User.class, id));
+
+          userRepository.delete(user);
+
+        return genericMessage("User with id %s deleted".formatted(id));
+    }
+
+    
+    @Operation(summary = "Toggle the admin field")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/toggleAdmin")
+    public Object toggleAdmin( @Parameter(name = "id", description = "Long, id number of user to toggle their admin field", example = "1", required = true) @RequestParam Long id){
+        User user = userRepository.findById(id)
+        .orElseThrow(() -> new EntityNotFoundException(User.class, id));
+
+        user.setAdmin(!user.getAdmin());
+        userRepository.save(user);
+        return genericMessage("User with id %s has toggled admin status to %s".formatted(id, user.getAdmin()));
+    }
+
+
+    @Operation(summary = "Toggle the driver field")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/toggleDriver")
+    public Object toggleDriver( @Parameter(name = "id", description = "Long, id number of user to toggle their driver field", example = "1", required = true) @RequestParam Long id){
+
+        User user = userRepository.findById(id)
+        .orElseThrow(() -> new EntityNotFoundException(User.class, id));
+
+        user.setDriver(!user.getDriver());
+        userRepository.save(user);
+        return genericMessage("User with id %s has toggled driver status to %s".formatted(id, user.getDriver()));
+    }
+
+    @Operation(summary = "Toggle the rider field")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/toggleRider")
+    public Object toggleRider( @Parameter(name = "id") @RequestParam Long id){
+        User user = userRepository.findById(id)
+        .orElseThrow(() -> new EntityNotFoundException(User.class, id));
+
+        user.setRider(!user.getRider());
+        userRepository.save(user);
+        return genericMessage("User with id %s has toggled rider status to %s".formatted(id, user.getRider()));
     }
 }
