dc5c18cc31dd8fdbf6a25896740c8ce32e64aafd
Logan Detrick <logandetrick@ucsb.edu>
2024-11-21 14:29:52 -0800

    ld - fix prettier

--- frontend/src/main/pages/RequestTypes/RequestTypeCreatePage.js
+++ frontend/src/main/pages/RequestTypes/RequestTypeCreatePage.js
@@ -1,5 +1,5 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-import RequestTypeForm from "main/components/RequestTypes/RequestTypeForm"
+import RequestTypeForm from "main/components/RequestTypes/RequestTypeForm";
 import { Navigate } from "react-router-dom";
 import { useBackendMutation } from "main/utils/useBackend";
 import { toast } from "react-toastify";
