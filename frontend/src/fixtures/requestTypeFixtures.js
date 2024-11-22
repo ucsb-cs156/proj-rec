05e2d7aa1fe2199654b135a746937ad189905618
Bruhwilliam <will1am.lau5264@gmail.com>
2024-11-21 17:54:00 -0800

    Added RequestTypeForm files, and created requestType fixtures

---
+++ frontend/src/fixtures/requestTypeFixtures.js
@@ -0,0 +1,40 @@
+const restaurantFixtures = {
+    oneRestaurant: [
+      {
+        id: 1,
+        name: "CS Department BS/MS program",
+        description: "Request for recommendation for CS Department BS/MS program",
+      },
+    ],
+  
+    threeRestaurants: [
+      {
+        id: 2,
+        name: "Scholarship or Fellowship",
+        description:
+          "Request for recommendation for a scholarship or fellowship",
+      },
+  
+      {
+        id: 3,
+        name: "MS program (other than CS Dept BS/MS)",
+        description: "Request for recommendation for MS program",
+      },
+  
+      {
+        id: 4,
+        name: "PhD program",
+        description:
+          "Request for recommendation for PhD program",
+      },
+      {
+        id: 5,
+        name: "Other",
+        description:
+          "Any other request type not listed"
+      },
+    ],
+  };
+  
+  export { requestTypeFixtures };
+  
---
+++ frontend/src/stories/components/RequestType/RequestTypeForm.stories.js
@@ -0,0 +1,33 @@
+import React from "react";
+import RequestTypeForm from "main/components/RequestType/RequestTypeForm";
+import { requestTypeFixtures } from "fixtures/requestTypeFixtures";
+
+export default {
+  title: "components/RequestTypes/RequestTypeForm",
+  component: RequestTypeForm,
+};
+
+const Template = (args) => {
+  return <RequestTypeForm {...args} />;
+};
+
+export const Create = Template.bind({});
+
+Create.args = {
+  buttonLabel: "Create",
+  submitAction: (data) => {
+    console.log("Submit was clicked with data: ", data);
+    window.alert("Submit was clicked with data: " + JSON.stringify(data));
+  },
+};
+
+export const Update = Template.bind({});
+
+Update.args = {
+  initialContents: requestTypeFixtures.oneRequestType[0],
+  buttonLabel: "Update",
+  submitAction: (data) => {
+    console.log("Submit was clicked with data: ", data);
+    window.alert("Submit was clicked with data: " + JSON.stringify(data));
+  },
+};
