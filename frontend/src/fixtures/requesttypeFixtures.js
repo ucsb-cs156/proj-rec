426ec63781f47db192efd29d91cb4bb6159a4c3f
chriswlai <ch94518@gmail.com>
2024-11-21 17:34:03 -0800

    add fixtures and form component

---
+++ frontend/src/fixtures/requesttypeFixtures.js
@@ -0,0 +1,27 @@
+const requesttypeFixtures = {
+    oneRequestType: [
+      {
+        id: 1,
+        requestType: "CS Department BS/MS program",
+      },
+    ],
+  
+    threeRequestTypes: [
+      {
+        id: 2,
+        requestType: "Scholarship or Fellowship",
+      },
+  
+      {
+        id: 3,
+        requestType: "MS program (other than CS Dept BS/MS)",
+      },
+  
+      {
+        id: 4,
+        requestType: "PhD program",
+      },
+    ],
+  };
+  
+  export { requesttypeFixtures };  
