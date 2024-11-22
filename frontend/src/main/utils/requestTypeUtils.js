60aa7def349853574c15630665b270a842dbc12d
Baige He <baige_he@163.com>
2024-11-21 16:09:47 -0800

    add utils and tests for requestType

---
+++ frontend/src/main/utils/requestTypeUtils.js
@@ -0,0 +1,20 @@
+import { toast } from "react-toastify";
+
+// Stryker disable all : dev facing debug function, no need to test
+
+export function onDeleteSuccess(message) {
+  console.log(message);
+  toast(message);
+}
+
+// Stryker restore all
+
+export function cellToAxiosParamsDelete(cell) {
+  return {
+    url: "/api/requesttype",
+    method: "DELETE",
+    params: {
+      id: cell.row.values.id,
+    },
+  };
+}
