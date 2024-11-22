e3863381db341dfae438c21302e4956cf5e490be
Michael Jiang <michaeljiang@ucsb.edu>
2024-11-21 17:22:52 -0800

    for rebasing

--- src/main/java/edu/ucsb/cs156/rec/repositories/UserRepository.java
+++ src/main/java/edu/ucsb/cs156/rec/repositories/UserRepository.java
@@ -14,9 +14,14 @@ public interface UserRepository extends CrudRepository<User, Long> {
   /**
    * This method returns a User entity with a given email.
    * @param email email address of the user
-   * @param name name of user
    * @return Optional of User (empty if not found)
    */
   Optional<User> findByEmail(String email);
+  
+  /**
+   * This method returns a User entity with a given email.
+   * @param name email address of the user
+   * @return Optional of User (empty if not found)
+   */
   Optional<User> findByName(String name);
 }
