ecb2f87e222040b5f4300a197c4a879e0b58fb67
Xinghan Yang <xinghan@umail.ucsb.edu>
2024-11-22 16:27:45 -0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,6 @@ function App() {
         {hasRole(currentUser, "ROLE_ADMIN") && (
           <Route exact path="/admin/users" element={<AdminUsersPage />} />
         )}
-
       </Routes>
     </BrowserRouter>
   );
