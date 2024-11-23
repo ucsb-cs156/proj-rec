4e4dad17867e3cca8d1d8aea22af4fb585eb6ed0
obenedek20 <obenedek20@users.noreply.github.com>
2024-11-23 12:49:23 -0800

    prettier

--- frontend/src/main/components/RecommendationRequest/RecommendationRequestForm.js
+++ frontend/src/main/components/RecommendationRequest/RecommendationRequestForm.js
@@ -1,141 +1,140 @@
 import { Button, Form, Row, Col } from "react-bootstrap";
 
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
-  import { useForm } from "react-hook-form";
-  import { useNavigate } from "react-router-dom";
+function RecommendationRequestForm({
+  initialContents,
+  submitAction,
+  buttonLabel = "Create",
+}) {
+  // Stryker disable all
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({ defaultValues: initialContents || {} });
+  // Stryker restore all
 
-  function RecommendationRequestForm({
-    initialContents,
-    submitAction,
-    buttonLabel = "Create",
-  }) {
-    // Stryker disable all
-    const {
-      register,
-      formState: { errors },
-      handleSubmit,
-    } = useForm({ defaultValues: initialContents || {} });
-    // Stryker restore all
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  // For explanation, see: https://stackoverflow.com/questions/3143070/javascript-regex-iso-datetime
+  // Note that even this complex regex may still need some tweaks
 
-    // For explanation, see: https://stackoverflow.com/questions/3143070/javascript-regex-iso-datetime
-    // Note that even this complex regex may still need some tweaks
+  // Stryker disable Regex
 
-    // Stryker disable Regex
-
-    return (
-      <Form onSubmit={handleSubmit(submitAction)}>
-        <Row>
-          {initialContents && (
-            <Col>
-              <Form.Group className="mb-3">
-                <Form.Label htmlFor="id">Id</Form.Label>
-                <Form.Control
-                  data-testid="RecommendationRequestForm-id"
-                  id="id"
-                  type="text"
-                  {...register("id")}
-                  value={initialContents.id}
-                  disabled
-                />
-              </Form.Group>
-            </Col>
-          )}
-
+  return (
+    <Form onSubmit={handleSubmit(submitAction)}>
+      <Row>
+        {initialContents && (
           <Col>
             <Form.Group className="mb-3">
-              <Form.Label htmlFor="professorName">Professor Name</Form.Label>
+              <Form.Label htmlFor="id">Id</Form.Label>
               <Form.Control
-                data-testid="RecommendationRequestForm-professorName"
-                id="professorName"
+                data-testid="RecommendationRequestForm-id"
+                id="id"
                 type="text"
-                isInvalid={Boolean(errors.professorName)}
-                {...register("professorName", {
-                  required: true,
-                })}
+                {...register("id")}
+                value={initialContents.id}
+                disabled
               />
-              <Form.Control.Feedback type="invalid">
-                {errors.professorName && "Professor Name is required. "}
-              </Form.Control.Feedback>
             </Form.Group>
           </Col>
-          <Col>
-            <Form.Group className="mb-3">
-              <Form.Label htmlFor="professorEmail">Professor Email</Form.Label>
-              <Form.Control
-                data-testid="RecommendationRequestForm-professorEmail"
-                id="professorEmail"
-                type="email"
-                isInvalid={Boolean(errors.professorEmail)}
-                {...register("professorEmail", {
-                  required: true,
-                })}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.professorEmail && "Professor Email is required. "}
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Col>
-        </Row>
+        )}
 
-        <Row>
-          <Col>
-            <Form.Group className="mb-3">
-              <Form.Label htmlFor="recommendationTypes">
-                Recommendation Types
-              </Form.Label>
-              <Form.Select
-                data-testid="RecommendationRequestForm-recommendationTypes"
-                id="recommendationTypes"
-                type="string"
-                isInvalid={Boolean(errors.recommendationTypes)}
-                {...register("recommendationTypes", {})}
-              >
-                <option value="CS Department BS/MS program">
-                  CS Department BS/MS program
-                </option>
-                <option value="Scholarship or Fellowship">
-                  Scholarship or Fellowship
-                </option>
-                <option value="MS program (other than CS Dept BS/MS)">
-                  MS program (other than CS Dept BS/MS)
-                </option>
-                <option value="PhD program">PhD program</option>
-                <option value="Other">Other</option>
-              </Form.Select>
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group className="mb-3">
-              <Form.Label htmlFor="details">Details</Form.Label>
-              <Form.Control
-                data-testid="RecommendationRequestForm-details"
-                id="details"
-                type="text"
-                isInvalid={Boolean(errors.details)}
-                {...register("details")}
-              />
-            </Form.Group>
-          </Col>
-        </Row>
+        <Col>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="professorName">Professor Name</Form.Label>
+            <Form.Control
+              data-testid="RecommendationRequestForm-professorName"
+              id="professorName"
+              type="text"
+              isInvalid={Boolean(errors.professorName)}
+              {...register("professorName", {
+                required: true,
+              })}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.professorName && "Professor Name is required. "}
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Col>
+        <Col>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="professorEmail">Professor Email</Form.Label>
+            <Form.Control
+              data-testid="RecommendationRequestForm-professorEmail"
+              id="professorEmail"
+              type="email"
+              isInvalid={Boolean(errors.professorEmail)}
+              {...register("professorEmail", {
+                required: true,
+              })}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.professorEmail && "Professor Email is required. "}
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Col>
+      </Row>
 
-        <Row>
-          <Col>
-            <Button type="submit" data-testid="RecommendationRequestForm-submit">
-              {buttonLabel}
-            </Button>
-            <Button
-              variant="Secondary"
-              onClick={() => navigate(-1)}
-              data-testid="RecommendationRequestForm-cancel"
+      <Row>
+        <Col>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="recommendationTypes">
+              Recommendation Types
+            </Form.Label>
+            <Form.Select
+              data-testid="RecommendationRequestForm-recommendationTypes"
+              id="recommendationTypes"
+              type="string"
+              isInvalid={Boolean(errors.recommendationTypes)}
+              {...register("recommendationTypes", {})}
             >
-              Cancel
-            </Button>
-          </Col>
-        </Row>
-      </Form>
-    );
-  }
+              <option value="CS Department BS/MS program">
+                CS Department BS/MS program
+              </option>
+              <option value="Scholarship or Fellowship">
+                Scholarship or Fellowship
+              </option>
+              <option value="MS program (other than CS Dept BS/MS)">
+                MS program (other than CS Dept BS/MS)
+              </option>
+              <option value="PhD program">PhD program</option>
+              <option value="Other">Other</option>
+            </Form.Select>
+          </Form.Group>
+        </Col>
+        <Col>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="details">Details</Form.Label>
+            <Form.Control
+              data-testid="RecommendationRequestForm-details"
+              id="details"
+              type="text"
+              isInvalid={Boolean(errors.details)}
+              {...register("details")}
+            />
+          </Form.Group>
+        </Col>
+      </Row>
 
-  export default RecommendationRequestForm;
+      <Row>
+        <Col>
+          <Button type="submit" data-testid="RecommendationRequestForm-submit">
+            {buttonLabel}
+          </Button>
+          <Button
+            variant="Secondary"
+            onClick={() => navigate(-1)}
+            data-testid="RecommendationRequestForm-cancel"
+          >
+            Cancel
+          </Button>
+        </Col>
+      </Row>
+    </Form>
+  );
+}
+
+export default RecommendationRequestForm;
--- frontend/src/stories/components/RecommendationRequest/RecommendationRequestForm.stories.js
+++ frontend/src/stories/components/RecommendationRequest/RecommendationRequestForm.stories.js
@@ -1,33 +1,33 @@
 import React from "react";
-  import { recommendationRequestFixtures } from "../../../fixtures/recommendationRequestFixtures";
-  import RecommendationRequestForm from "main/components/RecommendationRequest/RecommendationRequestForm";
+import { recommendationRequestFixtures } from "../../../fixtures/recommendationRequestFixtures";
+import RecommendationRequestForm from "main/components/RecommendationRequest/RecommendationRequestForm";
 
-  export default {
-    title: "components/RecommendationRequest/RecommendationRequestForm",
-    component: RecommendationRequestForm,
-  };
+export default {
+  title: "components/RecommendationRequest/RecommendationRequestForm",
+  component: RecommendationRequestForm,
+};
 
-  const Template = (args) => {
-    return <RecommendationRequestForm {...args} />;
-  };
+const Template = (args) => {
+  return <RecommendationRequestForm {...args} />;
+};
 
-  export const Create = Template.bind({});
+export const Create = Template.bind({});
 
-  Create.args = {
-    buttonLabel: "Create",
-    submitAction: (data) => {
-      console.log("Submit was clicked with data: ", data);
-      window.alert("Submit was clicked with data: " + JSON.stringify(data));
-    },
-  };
+Create.args = {
+  buttonLabel: "Create",
+  submitAction: (data) => {
+    console.log("Submit was clicked with data: ", data);
+    window.alert("Submit was clicked with data: " + JSON.stringify(data));
+  },
+};
 
-  export const Update = Template.bind({});
+export const Update = Template.bind({});
 
-  Update.args = {
-    initialContents: recommendationRequestFixtures.oneRecommendation[0],
-    buttonLabel: "Update",
-    submitAction: (data) => {
-      console.log("Submit was clicked with data: ", data);
-      window.alert("Submit was clicked with data: " + JSON.stringify(data));
-    },
-  };
+Update.args = {
+  initialContents: recommendationRequestFixtures.oneRecommendation[0],
+  buttonLabel: "Update",
+  submitAction: (data) => {
+    console.log("Submit was clicked with data: ", data);
+    window.alert("Submit was clicked with data: " + JSON.stringify(data));
+  },
+};
