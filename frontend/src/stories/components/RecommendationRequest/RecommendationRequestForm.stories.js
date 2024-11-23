56b65adc05335e548e3935ea1dc7c3a5a0b6aa1d
obenedek20 <obenedek20@users.noreply.github.com>
2024-11-23 12:44:57 -0800

    passing tests

---
+++ frontend/src/stories/components/RecommendationRequest/RecommendationRequestForm.stories.js
@@ -0,0 +1,33 @@
+import React from "react";
+  import { recommendationRequestFixtures } from "../../../fixtures/recommendationRequestFixtures";
+  import RecommendationRequestForm from "main/components/RecommendationRequest/RecommendationRequestForm";
+
+  export default {
+    title: "components/RecommendationRequest/RecommendationRequestForm",
+    component: RecommendationRequestForm,
+  };
+
+  const Template = (args) => {
+    return <RecommendationRequestForm {...args} />;
+  };
+
+  export const Create = Template.bind({});
+
+  Create.args = {
+    buttonLabel: "Create",
+    submitAction: (data) => {
+      console.log("Submit was clicked with data: ", data);
+      window.alert("Submit was clicked with data: " + JSON.stringify(data));
+    },
+  };
+
+  export const Update = Template.bind({});
+
+  Update.args = {
+    initialContents: recommendationRequestFixtures.oneRecommendation[0],
+    buttonLabel: "Update",
+    submitAction: (data) => {
+      console.log("Submit was clicked with data: ", data);
+      window.alert("Submit was clicked with data: " + JSON.stringify(data));
+    },
+  };
---
+++ frontend/src/tests/components/RecommendationRequest/RecommendationRequestForm.test.js
@@ -0,0 +1,104 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+
+
+
+
+
+
+  import { BrowserRouter as Router } from "react-router-dom";
+
+  import RecommendationRequestForm from "main/components/RecommendationRequest/RecommendationRequestForm";
+  import { recommendationRequestFixtures } from "fixtures/recommendationRequestFixtures";
+
+  import { QueryClient, QueryClientProvider } from "react-query";
+
+  const mockedNavigate = jest.fn();
+
+  jest.mock("react-router-dom", () => ({
+    ...jest.requireActual("react-router-dom"),
+    useNavigate: () => mockedNavigate,
+  }));
+
+  describe("RecommendationRequestForm tests", () => {
+    const queryClient = new QueryClient();
+
+    const expectedHeaders = [
+      "Professor Name",
+      "Professor Email",
+      "Recommendation Types",
+      "Details",
+    ];
+    const testId = "RecommendationRequestForm";
+
+    test("renders correctly with no initialContents", async () => {
+      render(
+        <QueryClientProvider client={queryClient}>
+          <Router>
+            <RecommendationRequestForm />
+          </Router>
+        </QueryClientProvider>,
+      );
+
+      expect(await screen.findByText(/Create/)).toBeInTheDocument();
+
+      expectedHeaders.forEach((headerText) => {
+        const header = screen.getByText(headerText);
+        expect(header).toBeInTheDocument();
+      });
+    });
+
+    test("renders correctly when passing in initialContents", async () => {
+      render(
+        <QueryClientProvider client={queryClient}>
+          <Router>
+            <RecommendationRequestForm
+              initialContents={recommendationRequestFixtures.oneRecommendation}
+            />
+          </Router>
+        </QueryClientProvider>,
+      );
+
+      expect(await screen.findByText(/Create/)).toBeInTheDocument();
+
+      expectedHeaders.forEach((headerText) => {
+        const header = screen.getByText(headerText);
+        expect(header).toBeInTheDocument();
+      });
+
+      expect(await screen.findByTestId(`${testId}-id`)).toBeInTheDocument();
+      expect(screen.getByText(`Id`)).toBeInTheDocument();
+    });
+
+    test("that navigate(-1) is called when Cancel is clicked", async () => {
+      render(
+        <QueryClientProvider client={queryClient}>
+          <Router>
+            <RecommendationRequestForm />
+          </Router>
+        </QueryClientProvider>,
+      );
+      expect(await screen.findByTestId(`${testId}-cancel`)).toBeInTheDocument();
+      const cancelButton = screen.getByTestId(`${testId}-cancel`);
+
+      fireEvent.click(cancelButton);
+
+      await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith(-1));
+    });
+
+    test("that the correct validations are performed", async () => {
+      render(
+        <QueryClientProvider client={queryClient}>
+          <Router>
+            <RecommendationRequestForm />
+          </Router>
+        </QueryClientProvider>,
+      );
+
+      expect(await screen.findByText(/Create/)).toBeInTheDocument();
+      const submitButton = screen.getByText(/Create/);
+      fireEvent.click(submitButton);
+
+      await screen.findByText(/Professor Name is required/);
+      expect(screen.getByText(/Professor Email is required/)).toBeInTheDocument();
+    });
+  });
