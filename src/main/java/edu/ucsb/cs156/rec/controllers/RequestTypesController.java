a072065720cd1b4b689b8e5507b30245f6529794
Ben Asperheim <basperheim@ucsb.edu>
2024-11-23 18:45:14 -0800

    implemented professor's suggestions

--- src/main/java/edu/ucsb/cs156/rec/controllers/RequestTypesController.java
+++ src/main/java/edu/ucsb/cs156/rec/controllers/RequestTypesController.java
@@ -73,8 +73,7 @@ public class RequestTypesController extends ApiController {
 
     /**
      * Update a single request type
-     * 
-     * @param id  the name of the request type 
+     * @param id  the id of the request type 
      * @param incoming the new RequestType
      * @return the saved requesttype
      * @throws DuplicateArgumentException if the request type already exists.
