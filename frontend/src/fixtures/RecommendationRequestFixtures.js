abf5e15fde60d47325f34ebaec7159eaf94bf11b
Phill Conrad <pconrad@cs.ucsb.edu>
2024-11-24 11:25:40 -0800

    Update frontend/src/fixtures/RecommendationRequestFixtures.js

--- frontend/src/fixtures/RecommendationRequestFixtures.js
+++ frontend/src/fixtures/RecommendationRequestFixtures.js
@@ -24,7 +24,7 @@ const recommendationRequestFixtures = {
       completionDate: "",
       status: "PENDING",
       details: "test details",
-      recommendationTypes: "CS Department BS/MS program",
+      recommendationType: "CS Department BS/MS program",
       user_id: 2,
     },
 
