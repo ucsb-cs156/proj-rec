dcd1958729c97fe175f63bafc12b0623329115f5
Victor Prchlik <vprchlik@ucsb.edu>
2024-11-25 20:54:02 -0800

    ran prettier on one more file

--- frontend/src/fixtures/pendingrequestsFixtures.js
+++ frontend/src/fixtures/pendingrequestsFixtures.js
@@ -1,33 +1,32 @@
 const pendingrequestsFixtures = {
-    oneRequest: [
-      {
-        id: 1,
-        studentEmail: "student1@example.com",
-        status: "Pending",
-        requestDate: "2024-11-01T10:00:00",
-      },
-    ],
-    threeRequests: [
-      {
-        id: 1,
-        studentEmail: "student1@example.com",
-        status: "Pending",
-        requestDate: "2024-11-01T10:00:00",
-      },
-      {
-        id: 2,
-        studentEmail: "student2@example.com",
-        status: "Pending",
-        requestDate: "2024-11-02T12:00:00",
-      },
-      {
-        id: 3,
-        studentEmail: "student3@example.com",
-        status: "Accepted",
-        requestDate: "2024-11-03T14:00:00",
-      },
-    ],
-  };
-  
-  export { pendingrequestsFixtures };
-  
+  oneRequest: [
+    {
+      id: 1,
+      studentEmail: "student1@example.com",
+      status: "Pending",
+      requestDate: "2024-11-01T10:00:00",
+    },
+  ],
+  threeRequests: [
+    {
+      id: 1,
+      studentEmail: "student1@example.com",
+      status: "Pending",
+      requestDate: "2024-11-01T10:00:00",
+    },
+    {
+      id: 2,
+      studentEmail: "student2@example.com",
+      status: "Pending",
+      requestDate: "2024-11-02T12:00:00",
+    },
+    {
+      id: 3,
+      studentEmail: "student3@example.com",
+      status: "Accepted",
+      requestDate: "2024-11-03T14:00:00",
+    },
+  ],
+};
+
+export { pendingrequestsFixtures };
