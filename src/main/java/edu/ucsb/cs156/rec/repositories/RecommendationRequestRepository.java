62a74522a8291cae026cf323b9e10f786a896e5f
divya-subramonian25 <divya.subramonian25@gmail.com>
2024-11-26 01:01:39 -0800

    ds - updated entity, repository, and database migration files

--- src/main/java/edu/ucsb/cs156/rec/repositories/RecommendationRequestRepository.java
+++ src/main/java/edu/ucsb/cs156/rec/repositories/RecommendationRequestRepository.java
@@ -4,12 +4,20 @@ import edu.ucsb.cs156.rec.entities.RecommendationRequest;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
-/**
+/*
  * The RecommendationRequestRepository is a repository for RecommendationRequest entities.
  */
 
 @Repository
 public interface RecommendationRequestRepository extends CrudRepository<RecommendationRequest, Long> {
-    
+    /**
+   * This method returns all the RecommendationRequests with a given requesterId.
+   * @param id id of the requester
+   * @return Iterable of RecommendationRequests
+   */
+  Iterable<RecommendationRequest> findAllByRequesterId(long requesterId);
+  Optional<RecommendationRequest> findAllById(long id);
+
 }
