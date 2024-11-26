851956ba138a630119be666332d2d7b9e839af8a
Ishan Jain <ishanjain42@gmail.com>
2024-11-25 17:59:43 -0800

    request type dropdown initial

---
+++ frontend/src/main/components/RequestTypeDropdown/RequestTypeDropdown.js
@@ -0,0 +1,53 @@
+import { compareValues } from "main/utils/sortHelper";
+import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+const SingleRequestDropdown = ({
+  requests,
+  request,
+  setrequest,
+  controlId,
+  onChange = null,
+  label = "Request Area",
+}) => {
+  const localSearchrequest = localStorage.getItem(controlId);
+
+  const [requestState, setrequestState] = useState(
+    // Stryker disable next-line all : not sure how to test/mock local storage
+    localSearchrequest || request,
+  );
+
+  const handleRequestOnChange = (event) => {
+    localStorage.setItem(controlId, event.target.value);
+    setrequestState(event.target.value);
+    setrequest(event.target.value);
+    if (onChange != null) {
+      onChange(event);
+    }
+  };
+
+  requests.sort(compareValues("requestCode"));
+
+  return (
+    <Form.Group controlId={controlId}>
+      <Form.Label>{label}</Form.Label>
+      <Form.Control
+        as="select"
+        value={requestState}
+        onChange={handleRequestOnChange}
+      >
+        {requests.map(function (object) {
+          const requestCode = object.requestCode.replace(/ /g, "-");
+          const key = `${controlId}-option-${requestCode}`;
+          return (
+            <option key={key} data-testid={key} value={object.requestCode}>
+              {object.requestCode} - {object.requestTranslation}
+            </option>
+          );
+        })}
+      </Form.Control>
+    </Form.Group>
+  );
+};
+
+export default SingleRequestDropdown;
---
+++ frontend/src/stories/components/RequestTypeDropdown/RequestTypeDropdown.stories.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+import SingleRequestDropdown from "main/components/RequestTypeDropdown/RequestTypeDropdown";
+import {
+  oneRequest,
+  threeRequests,
+  allTheRequests,
+} from "fixtures/requestFixtures";
+
+export default {
+  title: "components/RequestTypeDropdown/RequestTypeDropdown",
+  component: SingleRequestDropdown,
+};
+
+const Template = (args) => {
+  const [requests, setRequests] = useState(args.requests[0]);
+
+  return (
+    <SingleRequestDropdown
+    requests={requests}
+      setRequests={setRequests}
+      controlId={"SampleControlId"}
+      label={"Request"}
+      {...args}
+    />
+  );
+};
+
+export const OneRequest = Template.bind({});
+OneRequest.args = {
+  requests: oneRequest,
+};
+
+export const ThreeRequests = Template.bind({});
+ThreeRequests.args = {
+    requests: threeRequests,
+};
+
+export const AllTheRequests = Template.bind({});
+AllTheRequests.args = {
+    requests: allTheRequests,
+};
