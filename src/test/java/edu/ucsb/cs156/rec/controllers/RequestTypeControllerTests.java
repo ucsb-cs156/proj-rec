fe83d15de2662d01f2b0287ee924648fa47edeb9
obenedek20 <obenedek20@users.noreply.github.com>
2024-11-26 19:22:14 -0800

    merge

--- src/test/java/edu/ucsb/cs156/rec/controllers/RequestTypeControllerTests.java
+++ src/test/java/edu/ucsb/cs156/rec/controllers/RequestTypeControllerTests.java
@@ -566,36 +566,7 @@ public class RequestTypeControllerTests extends ControllerTestCase {
                 Map<String, Object> json = responseToJson(response);
                 assertEquals("RequestType with id 67 not found", json.get("message"));
         }
-
-}package edu.ucsb.cs156.rec.controllers;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.servlet.MvcResult;
-
-import edu.ucsb.cs156.rec.ControllerTestCase;
-import edu.ucsb.cs156.rec.entities.RequestType;
-import edu.ucsb.cs156.rec.entities.User;
-import edu.ucsb.cs156.rec.repositories.RequestTypeRepository;
-import edu.ucsb.cs156.rec.testconfig.TestConfig;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import java.util.Arrays;
-
-@WebMvcTest(controllers = RequestTypeController.class)
-@Import(TestConfig.class)
-public class RequestTypeControllerTests extends ControllerTestCase  {
-    @MockBean
-    RequestTypeRepository requestTypeRepository;
-
-    @Test
+        @Test
     public void logged_out_users_cannot_get_all_request_types() throws Exception {
             mockMvc.perform(get("/api/requesttype/all"))
                             .andExpect(status().is(403)); // logged out users can't get all
@@ -613,4 +584,5 @@ public class RequestTypeControllerTests extends ControllerTestCase  {
             String responseString = response.getResponse().getContentAsString();
             assertEquals(expectedJson, responseString);
     }
+
 }
