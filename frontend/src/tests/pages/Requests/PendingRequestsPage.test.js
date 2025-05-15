e3def619be654a412395009981f714bf70543c48
Derek Kirschbaum <dgkirschbaum@ucsb.edu>
2025-05-15 16:28:45 -0700

    dk - added recommendation request table to pending requests page

--- frontend/src/tests/pages/Requests/PendingRequestsPage.test.js
+++ frontend/src/tests/pages/Requests/PendingRequestsPage.test.js
@@ -38,6 +38,6 @@ describe("PendingRequestsPage tests", () => {
     );
 
     // assert
-    await screen.findByText("Pending Requests page not yet implemented");
+    await screen.findByText("Pending Requests");
   });
 });
