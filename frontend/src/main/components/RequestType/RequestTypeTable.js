061dd258ef32841cdc28fbc7ab7817e9de3995c5
Bryce Inouye <binouye@ucsb.edu>
2025-05-15 20:43:06 -0700

    bi - request type (WIP)

---
+++ frontend/src/main/components/RequestType/RequestTypeTable.js
@@ -0,0 +1,63 @@
+import React from "react";
+import OurTable, { ButtonColumn } from "main/components/OurTable";
+
+import { useBackendMutation } from "main/utils/useBackend";
+import {
+  cellToAxiosParamsDelete,
+  onDeleteSuccess,
+} from "main/utils/RequestTypeUtils";
+import { useNavigate } from "react-router-dom";
+import { hasRole } from "main/utils/currentUser";
+
+export default function RequestTypeTable({ requestTypes, currentUser }) {
+  const navigate = useNavigate();
+
+  const editCallback = (cell) => {
+    navigate(`/requestTypes/edit/${cell.row.values.id}`);
+  };
+
+  // Stryker disable all : hard to test for query caching
+
+  const deleteMutation = useBackendMutation(
+    cellToAxiosParamsDelete,
+    { onSuccess: onDeleteSuccess },
+    ["/api/requestTypes/all"],
+  );
+  // Stryker restore all
+
+  // Stryker disable next-line all : TODO try to make a good test for this
+  const deleteCallback = async (cell) => {
+    deleteMutation.mutate(cell);
+  };
+
+  const columns = [
+    {
+      Header: "id",
+      accessor: "id", // accessor is the "key" in the data
+    },
+    {
+      Header: "Request Type",
+      accessor: "requestType",
+    },
+  ];
+
+  if (
+    hasRole(currentUser, "ROLE_ADMIN") ||
+    hasRole(currentUser, "ROLE_PROFESSOR")
+  ) {
+    columns.push(
+      ButtonColumn("Edit", "primary", editCallback, "RequestTypeTable"),
+    );
+    columns.push(
+      ButtonColumn("Delete", "danger", deleteCallback, "RequestTypeTable"),
+    );
+  }
+
+  return (
+    <OurTable
+      data={requestTypes}
+      columns={columns}
+      testid={"RequestTypeTable"}
+    />
+  );
+}
---
+++ frontend/src/stories/components/RequestType/RequestTypeTable.stories.js
@@ -0,0 +1,41 @@
+import React from "react";
+import RequestTypeTable from "main/components/RequestType/RequestTypeTable";
+import requestTypeFixtures from "fixtures/requestTypeFixtures";
+import { currentUserFixtures } from "fixtures/currentUserFixtures";
+import { http, HttpResponse } from "msw";
+
+export default {
+  title: "components/RequestType/RequestTypeTable",
+  component: RequestTypeTable,
+};
+
+const Template = (args) => {
+  return <RequestTypeTable {...args} />;
+};
+
+export const Empty = Template.bind({});
+
+Empty.args = {
+  requestTypes: [],
+};
+
+export const FourTypesOrdinaryUser = Template.bind({});
+
+FourTypesOrdinaryUser.args = {
+  requestTypes: requestTypeFixtures.fourTypes,
+  currentUser: currentUserFixtures.userOnly,
+};
+
+export const FourTypesAdminUser = Template.bind({});
+FourTypesAdminUser.args = {
+  requestTypes: requestTypeFixtures.fourTypes,
+  currentUser: currentUserFixtures.adminUser,
+};
+
+FourTypesAdminUser.parameters = {
+  msw: [
+    http.delete("/api/requestTypes", () => {
+      return HttpResponse.json({}, { status: 200 });
+    }),
+  ],
+};
