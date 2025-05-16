2713f15cba1845248afb7dde014ce679944c7512
Tyler Wong <tyler_wong@ucsb.edu>
2025-05-16 10:33:00 -0700

    Admin users can see all requests + tests

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "main/pages/HomePage";
 import ProfilePage from "main/pages/ProfilePage";
 import AdminUsersPage from "main/pages/AdminUsersPage";
+import AdminRequestsPage from "main/pages/AdminRequestsPage";
 
 import PendingRequestsPage from "main/pages/Requests/PendingRequestsPage";
 import CompletedRequestsPage from "main/pages/Requests/CompletedRequestsPage";
@@ -21,28 +22,31 @@ function App() {
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/profile" element={<ProfilePage />} />
         {hasRole(currentUser, "ROLE_ADMIN") && (
-          <Route exact path="/admin/users" element={<AdminUsersPage />} />
+          <>
+            <Route exact path="/admin/users" element={<AdminUsersPage />} />
+            <Route exact path="/admin/requests" element={<AdminRequestsPage />} />
+          </>
         )}
         {(hasRole(currentUser, "ROLE_PROFESSOR") ||
           hasRole(currentUser, "ROLE_STUDENT")) && (
-          <>
-            <Route
-              exact
-              path="/requests/pending"
-              element={<PendingRequestsPage />}
-            />
-            <Route
-              exact
-              path="/requests/completed"
-              element={<CompletedRequestsPage />}
-            />
-            <Route
-              exact
-              path="/requests/statistics"
-              element={<StatisticsPage />}
-            />
-          </>
-        )}
+            <>
+              <Route
+                exact
+                path="/requests/pending"
+                element={<PendingRequestsPage />}
+              />
+              <Route
+                exact
+                path="/requests/completed"
+                element={<CompletedRequestsPage />}
+              />
+              <Route
+                exact
+                path="/requests/statistics"
+                element={<StatisticsPage />}
+              />
+            </>
+          )}
       </Routes>
     </BrowserRouter>
   );
