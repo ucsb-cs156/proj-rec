1a496c6785bdf5b68d534396268aac701aa3aec8
Lucas Havemose <lah@ucsb.edu>
2025-05-19 19:15:58 -0700

    everything except a single mutation test working

--- src/main/java/edu/ucsb/cs156/rec/controllers/RequestTypeController.java
+++ src/main/java/edu/ucsb/cs156/rec/controllers/RequestTypeController.java
@@ -37,6 +37,15 @@ public class RequestTypeController extends ApiController {
     @Autowired
     RequestTypeRepository requestTypeRepository;
 
+    @Autowired
+    public void startupValues() {
+        String[] values = {"CS Department BS/MS program", "Scholarship or Fellowship", "MS program (other than CS Dept BS/MS)", "PhD program", "Other"};
+        for (String value : values) {
+            if (!requestTypeRepository.findByRequestType(value).isPresent()) {
+                requestTypeRepository.save(RequestType.builder().requestType(value).build());
+            }
+        }
+    }
 
     /**
      * This method returns a list of all Request Types.
