a4b8b152249f588fdccca1aeee7220973f017b8d
Karen Yuan <karenldyuan@gmail.com>
2025-05-19 18:18:04 -0700

    removed commented out code in Request Type Form file

--- frontend/src/main/components/RequestType/RequestTypeForm.js
+++ frontend/src/main/components/RequestType/RequestTypeForm.js
@@ -19,11 +19,6 @@ function RequestTypeForm({
 
   const testIdPrefix = "RequestTypeForm";
 
-  // // Stryker disable Regex
-  // const isodate_regex =
-  //   /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
-  // // Stryker restore Regex
-
   return (
     <Form onSubmit={handleSubmit(submitAction)}>
       {initialContents && (
