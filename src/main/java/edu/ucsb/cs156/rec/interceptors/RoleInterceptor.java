66ef28a777282be227b98b737337f38be1670f37
Jeffrey Lin <jeffreylin@ucsb.edu>
2025-05-20 17:42:34 -0700

    removed the role_student from the backend

--- src/main/java/edu/ucsb/cs156/rec/interceptors/RoleInterceptor.java
+++ src/main/java/edu/ucsb/cs156/rec/interceptors/RoleInterceptor.java
@@ -48,8 +48,7 @@ public class RoleInterceptor implements HandlerInterceptor {
                 Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
                 Set<GrantedAuthority> revisedAuthorities = authorities.stream().filter(
                         grantedAuth -> !grantedAuth.getAuthority().equals("ROLE_ADMIN")
-                                && !grantedAuth.getAuthority().equals("ROLE_PROFESSOR")
-                                && !grantedAuth.getAuthority().equals("ROLE_STUDENT"))
+                                && !grantedAuth.getAuthority().equals("ROLE_PROFESSOR"))
                         .collect(Collectors.toSet());
                 if (user.getAdmin()) {
                     revisedAuthorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
@@ -58,7 +57,7 @@ public class RoleInterceptor implements HandlerInterceptor {
                     revisedAuthorities.add(new SimpleGrantedAuthority("ROLE_PROFESSOR"));
                 }
                 if (user.getStudent()) {
-                    revisedAuthorities.add(new SimpleGrantedAuthority("ROLE_STUDENT"));
+                    revisedAuthorities.add(new SimpleGrantedAuthority("ROLE_USER"));
                 }
                 Authentication newAuth = new OAuth2AuthenticationToken(principal, revisedAuthorities,
                         (((OAuth2AuthenticationToken) authentication).getAuthorizedClientRegistrationId()));
--- src/test/java/edu/ucsb/cs156/rec/interceptors/RoleInterceptorTests.java
+++ src/test/java/edu/ucsb/cs156/rec/interceptors/RoleInterceptorTests.java
@@ -141,13 +141,10 @@ public class RoleInterceptorTests extends ControllerTestCase{
             .anyMatch(grantedAuth -> grantedAuth.getAuthority().equals("ROLE_ADMIN"));
         boolean role_professor = authorities.stream()
             .anyMatch(grantedAuth -> grantedAuth.getAuthority().equals("ROLE_PROFESSOR"));
-        boolean role_student = authorities.stream()
-            .anyMatch(grantedAuth -> grantedAuth.getAuthority().equals("ROLE_STUDENT"));
         boolean role_user = authorities.stream()
             .anyMatch(grantedAuth -> grantedAuth.getAuthority().equals("ROLE_USER"));
         assertTrue(role_admin, "ROLE_ADMIN should be in roles list");
         assertTrue(role_professor, "ROLE_PROFESSOR should be in roles list");
-        assertTrue(role_student, "ROLE_STUDENT should be in roles list");
         assertTrue(role_user, "ROLE_USER should be in roles list");
 
     }
@@ -185,13 +182,10 @@ public class RoleInterceptorTests extends ControllerTestCase{
             .anyMatch(grantedAuth -> grantedAuth.getAuthority().equals("ROLE_ADMIN"));
         boolean role_professor = authorities.stream()
             .anyMatch(grantedAuth -> grantedAuth.getAuthority().equals("ROLE_PROFESSOR"));
-        boolean role_student = authorities.stream()
-            .anyMatch(grantedAuth -> grantedAuth.getAuthority().equals("ROLE_STUDENT"));
         boolean role_user = authorities.stream()
             .anyMatch(grantedAuth -> grantedAuth.getAuthority().equals("ROLE_USER"));
         assertFalse(role_admin, "ROLE_ADMIN should not be in roles list");
         assertTrue(role_professor, "ROLE_PROFESSOR should be in roles list");
-        assertFalse(role_student, "ROLE_STUDENT should not be in roles list");
         assertTrue(role_user, "ROLE_USER should be in roles list");
     }
 
@@ -228,13 +222,10 @@ public class RoleInterceptorTests extends ControllerTestCase{
             .anyMatch(grantedAuth -> grantedAuth.getAuthority().equals("ROLE_ADMIN"));
         boolean role_professor = authorities.stream()
             .anyMatch(grantedAuth -> grantedAuth.getAuthority().equals("ROLE_PROFESSOR"));
-        boolean role_student = authorities.stream()
-            .anyMatch(grantedAuth -> grantedAuth.getAuthority().equals("ROLE_STUDENT"));
         boolean role_user = authorities.stream()
             .anyMatch(grantedAuth -> grantedAuth.getAuthority().equals("ROLE_USER"));
         assertTrue(role_admin, "ROLE_ADMIN should be in roles list");
         assertTrue(role_professor, "ROLE_PROFESSOR should be in roles list");
-        assertTrue(role_student, "ROLE_STUDENT should be in roles list");
         assertTrue(role_user, "ROLE_USER should be in roles list");
     }
 
@@ -271,13 +262,10 @@ public class RoleInterceptorTests extends ControllerTestCase{
             .anyMatch(grantedAuth -> grantedAuth.getAuthority().equals("ROLE_ADMIN"));
         boolean role_professor = authorities.stream()
             .anyMatch(grantedAuth -> grantedAuth.getAuthority().equals("ROLE_PROFESSOR"));
-        boolean role_student = authorities.stream()
-            .anyMatch(grantedAuth -> grantedAuth.getAuthority().equals("ROLE_STUDENT"));
         boolean role_user = authorities.stream()
             .anyMatch(grantedAuth -> grantedAuth.getAuthority().equals("ROLE_USER"));
         assertTrue(role_admin, "ROLE_ADMIN should be in roles list");
         assertFalse(role_professor, "ROLE_PROFESSOR should not be in roles list");
-        assertFalse(role_student, "ROLE_STUDENT should be not in roles list");
         assertTrue(role_user, "ROLE_USER should be in roles list");
     }
             
