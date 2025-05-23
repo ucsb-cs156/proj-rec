868b563335a75c865837e8674b76b903c25c778f
Kenneth Tan <kennethtan@ucsb.edu>
2025-05-23 13:02:01 -0700

    fixed accidental deletion

--- frontend/src/tests/utils/RecommendationRequestUtils.test.js
+++ frontend/src/tests/utils/RecommendationRequestUtils.test.js
@@ -51,17 +51,19 @@ describe("RecommendationRequestUtils", () => {
       });
     });
   });
-  
-  describe("formattedDate", () => {
-    test("It returns the correct date", () => {
-      // arrange
-      const val = "2023-10-01T12:34:56";
+});
 
-      // act
-      const result = formattedDate(val);
+describe("formattedDate", () => {
+  test("It returns the correct date", () => {
+    // arrange
+    const val = "2023-10-01T12:34:56";
 
-      // assert
-      expect(result).toEqual("10/01/2023 12:34");
+    // act
+    const result = formattedDate(val);
+
+    // assert
+    expect(result).toEqual("10/01/2023 12:34");
+  });
 
   describe("onUpdateStatusSuccess", () => {
     test("It puts the message on console.log and in a toast", () => {
