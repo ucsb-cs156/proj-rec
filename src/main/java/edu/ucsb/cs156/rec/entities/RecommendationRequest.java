c491f202cb8824167e9388ae7c595aabfd761c44
Lucas Havemose <lah@ucsb.edu>
2025-05-22 19:40:48 -0700

    this better work

--- src/main/java/edu/ucsb/cs156/rec/entities/RecommendationRequest.java
+++ src/main/java/edu/ucsb/cs156/rec/entities/RecommendationRequest.java
@@ -42,7 +42,7 @@ public class RecommendationRequest {
   private User professor;
 
   @ManyToOne
-  @JoinColumn(name = "recommendation_type", referencedColumnName = "id", insertable = true, updatable = true)
+  @JoinColumn(name = "recommendation_type", referencedColumnName = "requestType", insertable = true, updatable = true, unique = true)
   private RequestType recommendationType;
 
   private String details;
