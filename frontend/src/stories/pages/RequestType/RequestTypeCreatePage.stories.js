251316fe4087398b141436ebbd6806b18f73aff9
Cheng <cxi@ucsb.edu>
2025-05-22 22:23:10 -0700

    okay don't mess with variable names

--- frontend/src/stories/pages/RequestType/RequestTypeCreatePage.stories.js
+++ frontend/src/stories/pages/RequestType/RequestTypeCreatePage.stories.js
@@ -21,8 +21,8 @@ Default.parameters = {
     http.get("/api/systemInfo", () =>
       HttpResponse.json(systemInfoFixtures.showingNeither, { status: 200 }),
     ),
-    http.post("/api/requesttypes/post", ({ requesttype }) => {
-      const url = new URL(requesttype.url);
+    http.post("/api/requesttypes/post", ({ request }) => {
+      const url = new URL(request.url);
       const id = url.searchParams.get("id");
       const requestType = url.searchParams.get("requestType");
 
